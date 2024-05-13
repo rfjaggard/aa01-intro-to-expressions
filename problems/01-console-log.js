ba5bc5012c405c6fb426e749c7e09b18ad04c3c3
Rylan Jaggard <93746697+rfjaggard@users.noreply.github.com>
2024-05-13 16:10:24 -0700

    Add files via upload

---
+++ problems/01-console-log.js
@@ -0,0 +1,15 @@
+/*
+Here's the prompt for this problem file:
+
+Time to write your first program! Use `console.log` to print out a message.
+Closely follow the syntax of the given `console.log` example.
+*/
+
+// Here is an example of using console.log to print: Hello World!:
+
+console.log('Hello World!');
+
+// Print out your own message using console.log below:
+
+// Your code here 
+console.log('This message is brought to you by satellites in space!')
---
+++ problems/02-code-comments.js
@@ -0,0 +1,26 @@
+/*
+Use your newfound knowledge of comments to execute the prompts in this problem
+file.
+*/
+
+/* 1. This will print 'Welcome to App Academy!'  */
+
+console.log('Welcome to App Academy!'); //Question
+
+/* Print more messages to the terminal using console.log */
+
+// 2. This is a single line comment.
+// console.log('This is will not be printed to the terminal')
+// Try adding your own single line comments:
+ 
+console.log('How many do I need?') //Not enough yet.
+console.log('Lets go'); //Lets not.
+
+/* 3.
+This is a multi-line comment.
+Try adding your own multi-line comments:
+*/
+
+// Your code here 
+/* but I'm willing to go with it
+anyway. */
